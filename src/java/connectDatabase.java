d0769cd0e0d54976396268d2c6355c85eb1b0578
mcao9 <mcao9@ucsc.edu>
2020-01-18 22:02:49 -0800

    test

--- src/java/connectDatabase.java
+++ src/java/connectDatabase.java
@@ -1,9 +1,14 @@
 import java.sql.*;
 import java.io.*;
 import java.util.*;
+import com.fazecast.jSerialComm.*;
+
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class connectDatabase{
-    public static void main(String[] args){
+    public static void main(String[] args){     
+  
         Connection connection = null;
         try{
             Class.forName("org.postgresql.Driver");
@@ -13,6 +18,11 @@ public class connectDatabase{
             if (connection != null){
                 System.out.println("Connected to the cloud database!");
             }
+            updateDatabase ud = new updateDatabase(connection);
+            PlantStatSet ss = new PlantStatSet();
+            ss.readPort();
+  
+            System.out.println(ud.insertSensorInfo(ss.getLight(), ss.getSoilMoist(), ss.getCO2(), ss.gettVoc(), ss.getRain()));
         } catch (SQLException | ClassNotFoundException e){
             System.out.println("Error while connection to the cloud database: " + e);
             e.printStackTrace();
--- src/java/updateDatabase.java
+++ src/java/updateDatabase.java
@@ -12,20 +12,22 @@ public class updateDatabase {
     }
     public int insertSensorInfo(int light, int soilMoist, int CO2, int tVoc, int rain){
         int numOfRowsUpdated = 0;
-        while(numOfRowsUpdated != 60){
-            try {
-                String query = "INSERT INTO sensor VALUES (?, ?, ?, ?, ?)";
-                PreparedStatement statement = connection.prepareStatement(query);
-                statement.setInt(1, light);
-                statement.setInt(2, soilMoist);
-                statement.setInt(3, CO2);
-                statement.setInt(4, tVoc);
-                statement.setInt(5, rain);
+        try {
+            String query = "INSERT INTO sensor(lightLevel, moistureLevel, carbonDLevel, tVOCLevel, rainLevel) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, light);
+            statement.setInt(2, soilMoist);
+            statement.setInt(3, CO2);
+            statement.setInt(4, tVoc);
+            statement.setInt(5, rain);
+
+            statement.execute();
     
-                numOfRowsUpdated++;
-            } catch (SQLException e){
-                e.printStackTrace();
-            }
+            numOfRowsUpdated = statement.getUpdateCount();
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+        return numOfRowsUpdated;
     }
 }
